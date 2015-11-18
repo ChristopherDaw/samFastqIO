12e1eb2e4a9c5d916a0c1ec7f0edfd1b2436e455
Carlos Navarro Astiasarán <cnavast@MacBook-Pro-de-Carlos.local>
2015-11-17 18:09:35 -0800

    updates

---
+++ include/aux_data.h
@@ -0,0 +1,14 @@
+//
+//  aux_data.h
+//  samFastqIO
+//
+//  Created by Carlos Navarro Astiasarán on 14/10/15.
+//  Copyright (c) 2015 Carlos Navarro Astiasarán. All rights reserved.
+//
+
+#ifndef samFastqIO_aux_data_h
+#define samFastqIO_aux_data_h
+
+
+
+#endif
